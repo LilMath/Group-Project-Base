4c7daf03399018a2b5fecf8da1aa87f2d9650870
Matt <mproctor0204@gmail.com>
2022-12-12 21:42:06 -0500

    added api code

--- client/script.js
+++ client/script.js
@@ -1,24 +1,51 @@
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '89685e395fmsh3d2378d3ff83ae8p19e6f1jsn2886088315d9',
-    'X-RapidAPI-Host': 'calorieninjas.p.rapidapi.com'
-  }
-};
+/* eslint-disable no-new */
+function apiGet() {
+  // var for input
+  const input1 = document.getElementById('input1').value;
+  const url = `https://calorieninjas.p.rapidapi.com/v1/nutrition?query=${input1}`;
 
-fetch('https://calorieninjas.p.rapidapi.com/v1/nutrition?query=tomato', options)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    console.log(data.items);
-    console.log(data.items[0].name);
-    console.log(data.items[0].calories);
+  // header info
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '89685e395fmsh3d2378d3ff83ae8p19e6f1jsn2886088315d9',
+      'X-RapidAPI-Host': 'calorieninjas.p.rapidapi.com'
+    }
+  };
 
-    const cals = `<p>${data.items[0].calories}</p>`;
+  // request data
+  fetch(url, options)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.items[0]);
+      const {name} = data.items[0];
+      const {calories} = data.items[0];
+      const {sugar_g} = data.items[0];
+      const {protein_g} = data.items[0];
 
-    document.getElementById('test').innerHTML = cals;
-  })
-  .catch((err) => console.error(err));
-// .then((response) => response.json())
-// .then((response) => console.log(response))
-// .catch((err) => console.error(err));
+      const ctx = document.getElementById('myChart');
+
+      const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: ['Calories', 'Sugar', 'Protein', 'Green', 'Purple', 'Orange'],
+          datasets: [{
+            label: '# of Votes',
+            data: [calories, sugar_g, protein_g, 5, 2, 3],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      });
+    })
+    .catch((err) => console.error(err));
+  // .then((response) => response.json())
+  // .then((response) => console.log(response))
+  // .catch((err) => console.error(err));
+}
